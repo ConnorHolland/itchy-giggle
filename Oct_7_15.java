e14366d597ae06de1b6899ec098ebf9e2acfafcb
Connor Holland <hollandcc@student.swosu.edu>
2015-10-07 09:48:08 -0500

    laid the outline for Round 1 down

--- Oct_7_15.java
+++ Oct_7_15.java
@@ -4,5 +4,31 @@ public class Oct_7_15 {
 
 		System.out.println"(Hello Flargleschnarfs!");
 		
+		/* Outline for math game problem
+		 * 
+		 * Round 1
+		 * Generate 2 random numbers
+		 * Ask the user to add these two numbers together
+		 * Read in their response
+		 * Check if answer was correct
+		 * 		IF correct
+		 * 			Tell them it was correct
+		 * 			Give them points
+		 * 			Make the next question harder
+		 * 		IF not correct
+		 * 			Tell them it was wrong
+		 * 			Tell them the correct answer
+		 * 			Do not give them points
+		 * 			Make the next question easier
+		 * Round 2
+		 * Round 3
+		 * Round 4
+		 */
+		
+		
+		
+		
+		
+		
 	}
 }
